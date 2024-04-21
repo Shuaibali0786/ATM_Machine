0f4436d7d5a6657f53016eaff19d51ba2570ca1d
Shuaibali0786 <160752334+Shuaibali0786@users.noreply.github.com>
2024-04-21 19:58:42 +0500

    adding a atm machine project

---
+++ main.js
@@ -0,0 +1,41 @@
+import inquirer from "inquirer";
+let myBalance = 10000; // Doller
+let myPin = 1234;
+// Print a welcome message 
+console.log("Welcome to Shuaib - Atm Machine");
+let pinAnswer = await inquirer.prompt([
+    {
+        name: "pin",
+        message: "enter your pin",
+        type: "number"
+    }
+]);
+if (pinAnswer.pin === myPin) {
+    console.log("your pin code successfully!!!");
+    let operationAns = await inquirer.prompt([
+        {
+            name: "operation",
+            message: "please select option",
+            type: "list",
+            choices: ["withdraw", "check balance"]
+        }
+    ]);
+    console.log(operationAns);
+    if (operationAns.operation === "withdraw") {
+        let amountAns = await inquirer.prompt([
+            {
+                name: "amount",
+                message: "enter your amount",
+                type: "number"
+            }
+        ]);
+        myBalance -= amountAns.amount;
+        console.log("your remaing balanceis:" + myBalance);
+    }
+    else if (operationAns.operation === "check balance") {
+        console.log("your balance is: " + myBalance);
+    }
+}
+else {
+    console.log("Incorrect pin code");
+}
